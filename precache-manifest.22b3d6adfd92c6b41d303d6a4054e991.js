cb82488df7116221f797fcf3560f42ba59f4bc6d
aradgonen <gonena.arad@gmail.com>
2019-03-23 23:13:30 +0200

    Add files via upload

---
+++ precache-manifest.22b3d6adfd92c6b41d303d6a4054e991.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dca47c22988c95e09c3c862737380ea",
+    "url": "/static/media/about.4dca47c2.jpg"
+  },
+  {
+    "revision": "84b8481941bf9e4d379e",
+    "url": "/static/css/main.907dbb58.chunk.css"
+  },
+  {
+    "revision": "5b3aba2713dae563be0f46df5b08024c",
+    "url": "/static/media/HomePage.5b3aba27.png"
+  },
+  {
+    "revision": "41c5f1eb1d1bf7143cb9",
+    "url": "/static/js/1.41c5f1eb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25caef210b0a583609233858da07d0f0",
+    "url": "/static/media/meitar-logo.25caef21.jpg"
+  },
+  {
+    "revision": "84b8481941bf9e4d379e",
+    "url": "/static/js/main.84b84819.chunk.js"
+  },
+  {
+    "revision": "0770ca34dd2a35a71fe3e3216119aed8",
+    "url": "/static/media/therapyFields.0770ca34.png"
+  },
+  {
+    "revision": "31e41f93070dc7912b7b8d104e705e71",
+    "url": "/static/media/inspection.31e41f93.png"
+  },
+  {
+    "revision": "f03ee6ac6bdd1016a71f17e7d81d21fd",
+    "url": "/static/media/dictionary.f03ee6ac.png"
+  },
+  {
+    "revision": "41c5f1eb1d1bf7143cb9",
+    "url": "/static/css/1.c6da4a3a.chunk.css"
+  },
+  {
+    "revision": "5f47c040e1c2bf82da950fec4cb1d7ba",
+    "url": "/index.html"
+  }
+];
