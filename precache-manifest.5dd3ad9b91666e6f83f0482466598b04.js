92ff055045d2ac956bedc49370e265fcc55f352a
aradgonen <gonena.arad@gmail.com>
2019-10-26 10:21:33 +0300

    Updates

---
+++ precache-manifest.5dd3ad9b91666e6f83f0482466598b04.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e886d85f5b8518ac067af87f901b1daa",
+    "url": "/Meitar/index.html"
+  },
+  {
+    "revision": "12e9241376f886bc319e",
+    "url": "/Meitar/static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "3c330503b9f15dde9801",
+    "url": "/Meitar/static/css/main.8a4cd694.chunk.css"
+  },
+  {
+    "revision": "12e9241376f886bc319e",
+    "url": "/Meitar/static/js/2.d7f07d0e.chunk.js"
+  },
+  {
+    "revision": "3c330503b9f15dde9801",
+    "url": "/Meitar/static/js/main.c4ac1160.chunk.js"
+  },
+  {
+    "revision": "c1d676fec445ee05b341",
+    "url": "/Meitar/static/js/runtime-main.6689a909.js"
+  },
+  {
+    "revision": "5b3aba2713dae563be0f46df5b08024c",
+    "url": "/Meitar/static/media/HomePage.5b3aba27.png"
+  },
+  {
+    "revision": "4dca47c22988c95e09c3c862737380ea",
+    "url": "/Meitar/static/media/about.4dca47c2.jpg"
+  },
+  {
+    "revision": "f03ee6ac6bdd1016a71f17e7d81d21fd",
+    "url": "/Meitar/static/media/dictionary.f03ee6ac.png"
+  },
+  {
+    "revision": "31e41f93070dc7912b7b8d104e705e71",
+    "url": "/Meitar/static/media/inspection.31e41f93.png"
+  },
+  {
+    "revision": "25caef210b0a583609233858da07d0f0",
+    "url": "/Meitar/static/media/meitar-logo.25caef21.jpg"
+  },
+  {
+    "revision": "0770ca34dd2a35a71fe3e3216119aed8",
+    "url": "/Meitar/static/media/therapyFields.0770ca34.png"
+  }
+]);
